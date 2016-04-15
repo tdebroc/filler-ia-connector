0983c2fc2b25f3da716fa1c00dc3a660a43f6752
thibautdebroca <tdebroc@gmail.com>
2016-04-15 22:16:19 +0200

    IA versus IA

--- src/main/java/com/tdebroc/myapp/filler/game/Main.java
+++ src/main/java/com/tdebroc/myapp/filler/game/Main.java
@@ -13,9 +13,13 @@ public class Main {
     public static void main(String[] args) {
 
         // ManualIAVsOther();
-        onlyIA(44);
+        //onlyIA(44);
+        IAversusIA(2);
     }
 
+    //================================================
+    // Example 1: Simple IA subscribing
+    //================================================
     public static void onlyIA(int gameId) {
         System.out.println("Open a new game is " + gameId);
         PlayerConnector playerConnector1 = new PlayerConnector(gameId, baseUrl);
@@ -32,6 +36,9 @@ public class Main {
 
     }
 
+    //================================================
+    // Example 2: Manual IA playing.
+    //================================================
     public static void ManualIAVsOther() {
 
         int gameId = PlayerConnector.addGame(baseUrl, 5);
@@ -53,5 +60,31 @@ public class Main {
     }
 
 
+    //================================================
+    // Example 3: IA VS IA.
+    //================================================
+    public static void IAversusIA(int gameId) {
+        System.out.println("Open a new game is " + gameId);
 
+        PlayerConnector playerConnector1 = new PlayerConnector(gameId, baseUrl);
+        playerConnector1.registerPlayer("Carl");
+
+        PlayerConnector playerConnector2 = new PlayerConnector(gameId, baseUrl);
+        playerConnector2.registerPlayer("Bob");
+        Game game = playerConnector1.getGame();
+        IA simpleIA = new SimpleIA();
+        IA simpleIA2 = new SimpleIA();
+
+        do {
+            game = playerConnector1.getOpponentMoves();
+            char c = simpleIA.getNextMove(game);
+            playerConnector1.sendMove(c);
+
+            game = playerConnector1.getOpponentMoves();
+            c = simpleIA2.getNextMove(game);
+            playerConnector2.sendMove(c);
+
+        } while (!game.isFinished());
+
+    }
 }
